92bf7baeb4c2ea8c1ee9439af5bac5fc4bfa2b4b
Mikaelpro1010 <mkpro10@alu.ufc.br>
2024-05-12 18:50:19 -0300

    Modificando o nome do componente de Home para Chatbot

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { NavigationContainer} from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Login from './src/components/Login/';
-import Home from './src/components/Home/';
+import Home from './src/components/Chatbot';
 import Register from './src/components/Register/';
 
 const Stack = createStackNavigator();
@@ -14,7 +14,7 @@ const App = () => {
       <Stack.Navigator>
         <Stack.Screen name="Login" component={Login}/>
         <Stack.Screen name="Register" component={Register}/>
-        <Stack.Screen name="Home" component={Home}/>
+        <Stack.Screen name="Chatbot" component={Chatbot}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
